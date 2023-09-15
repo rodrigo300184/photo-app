9ca60c51de35487f864a7af629b8962710a36669
Rodrigo <rodrigo300184@gmail.com>
2023-09-15 17:38:55 +0200

    Adding GitHub - routes

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ root.render(
           <Route path="/" element={<App />} />
           <Route path="/search" element={<Search />} />
           <Route path="/favorites" element={<Favorites />} />
-          <Route path='/photo-app' element={<Home />} />
-          <Route path='*' element={<Home />} />
+          <Route path='/photo-app' element={<App />} />
+          <Route path='*' element={<App />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
